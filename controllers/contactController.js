19a2eff5a44bec0a3941d9ed56ed7ddbe2779fd3
Minhal <minhal@tekrevol.com>
2024-12-30 22:54:21 +0500

    Inital code

---
+++ controllers/contactController.js
@@ -0,0 +1,57 @@
+//@desc Get all contacts
+//@route GET /api/contacts
+//@access public
+const getContacts = (req, res) => {
+    res.status(200).json({
+        message: "Get all contacts"
+    });
+}
+
+//@desc Get single contact
+//@route GET /api/contacts
+//@access public
+const getContact = (req, res) => {
+    res.status(200).json({
+        message: `Get contact ${req.params.id}`
+    });
+}
+
+
+//@desc Create contact
+//@route POST /api/contacts
+//@access public
+const createContact = (req, res) => {
+    console.log(`The request body is`, req.body);
+
+
+    const { name, email, phone } = req.body;
+
+    if (!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+
+    res.status(201).json({
+        message: "Create a contact"
+    });
+}
+
+//@desc Update contact
+//@route PUT /api/contacts
+//@access public
+const updateContact = (req, res) => {
+    res.status(200).json({
+        "message": `Update contact ${req.params.id}`
+    });
+}
+
+//@desc Delete contact
+//@route DELETE /api/contacts
+//@access public
+const deleteContact = (req, res) => {
+    res.status(200).json({
+        "message": `Delete contact ${req.params.id}`
+    });
+}
+
+module.exports = { getContacts, getContact, createContact, updateContact, deleteContact };
